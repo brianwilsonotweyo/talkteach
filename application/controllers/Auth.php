df33be6cd96d6a346602ba85cfb82828dbc49c46
brian otweyo <brianotweyo@gmail.com>
2021-03-29 20:17:39 +0300

    wonder flare

---
+++ application/controllers/Auth.php
@@ -0,0 +1,19 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+
+class Auth extends CI_Controller
+{
+
+    function __construct()
+    {
+        parent::__construct();
+        $this->load->database();
+        $this->load->library('session');
+    }
+
+
+    public function index()
+    {
+        $this->load->view('back/account/login');
+    }
+}
